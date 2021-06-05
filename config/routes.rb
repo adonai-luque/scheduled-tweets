5e5e5ee01fac891eee095920cead4c9394a375fc
adonai-luque <akluqueprieto@gmail.com>
2021-06-05 03:26:19 -0500

    User Sign Up error handling

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@
 
 Rails.application.routes.draw do
   root to: "main#index"
-  get "/about", to: "about#index", as: :about
+  get "/about", to: "about#index"
+  get "/sign_up", to: "registrations#new"
+  post "/sign_up", to: "registrations#create"
 end
